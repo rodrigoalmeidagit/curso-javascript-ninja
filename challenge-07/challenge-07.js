43fe0e6de27ebcd9caf49d31829e53c17ffb2e3e
rodrigoalmeidagit <novators@gmail.com>
2020-12-16 11:04:41 -0300

    correção challenge-07

--- challenge-07/challenge-07.js
+++ challenge-07/challenge-07.js
@@ -2,16 +2,23 @@
 Crie um array com 5 items (tipos variados).
 */
 
-var myArray = ['Rodrigo', 41, {nome: 'Renato', idade: 40}, [10, 20, 30, 40], 'true'];
+var myArray = ['Rodrigo', 41, { nome: 'Renato', idade: 40 }, [10, 20, 30, 40], 'true'];
 
 /*
 Crie uma função chamada `addItem`, que irá adicionar itens no array criado.
 A função deverá retornar o array atualizado.
 */
 
-function addItem(item) {
-    myArray.push(item);
-    console.log(myArray);
+// function addItem(item) {
+//     myArray.push(item);
+//     console.log(myArray);
+// }
+
+// Correção:
+
+function addItem( item ) {
+    myArray.push( item );
+    return myArray;
 }
 
 /*
@@ -19,8 +26,12 @@ Adicione um novo array ao array criado no início do desafio, com ao menos 3
 itens de tipos diferentes, mostrando o resultado no console.
 */
 
-addItem(['Almeida', {sexo: 'Masculino'}, 50]);
-console.log(myArray);
+// addItem(['Almeida', { sexo: 'Masculino' }, 50]);
+// console.log(myArray);
+
+//Outra resolução:
+
+console.log(addItem(['Almeida', { sexo: 'Masculino' }, 50]));
 
 /*
 Mostre no console o segundo elemento desse último array, criado acima, com a
@@ -35,38 +46,53 @@ Mostre no console quantos itens tem o primeiro array criado, com a frase:
 "O primeiro array tem [QUANTIDADE DE ITENS] itens."
 */
 
-console.log(`O primeiro array tem ${myArray.length} itens.`)
+console.log(`O primeiro array tem ${myArray.length} itens.`);
 
 /*
 Agora mostre no console quantos itens tem o segundo array criado, com a frase:
 "O segundo array tem [QUANTIDADE DE ITENS] itens."
 */
 
-console.log(`O segundo array tem ${myArray[5].length} itens.`)
+console.log(`O segundo array tem ${myArray[5].length} itens.`);
 
 /*
 Utilizando a estrutura de repetição `while`, mostre no console todos os números
 pares entre 10 e 20, inclusive esses 2.
 */
 console.log('Números pares entre 10 e 20:');
+// var counter = 10;
+// while (counter <= 20) {
+//     if (counter % 2 === 0) {
+//         console.log(counter);
+//     }
+//     counter++;
+// }
+
+// Outra resolução:
 var counter = 10;
-while (counter <= 20 ) {
-  if(counter % 2 === 0) {
-    console.log(counter);
-  }
-  counter++;
+while ( counter <= 20 ) {
+    counter % 2 === 0 ? console.log ( counter ) : '';
+    counter++;
 }
+
 
 /*
 Na mesma ideia do exercício acima: mostre agora os números ímpares.
 */
 console.log('Números ímpares entre 10 e 20:');
+// var counter = 10;
+// while (counter <= 20) {
+//     if (counter % 2 !== 0) {
+//         console.log(counter);
+//     }
+//     counter++
+// }
+
+// Outra resolução:
 var counter = 10;
-while(counter <= 20) {
-  if(counter % 2 !== 0) {
-    console.log(counter);
-  }
-  counter++
+while ( counter <= 20 ) {
+    counter % 2 !== 0 ? console.log( counter ) : '';
+    counter++;
 }
 
 /*
@@ -76,18 +102,27 @@ Só vamos mudar o range:
 - No segundo "for", mostre os números ímpares entre 111 e 125, inclusive eles.
 */
 
-console.log( 'Números pares entre 100 e 120:' );
-for (var counter = 100; counter <= 120; counter++) {
-  if(counter % 2 === 0){
-    console.log(counter);
-  }
+console.log('Números pares entre 100 e 120:');
+// for (var counter = 100; counter <= 120; counter++) {
+//     if (counter % 2 === 0) {
+//         console.log(counter);
+//     }
+// }
+
+// Outra resolução:
+for ( var counter = 100; counter <= 120; counter++ ) {
+    counter % 2 === 0 ? console.log( counter ) : '';
 }
 
 //
-console.log( 'Números ímpares entre 111 e 125:' );
-for(var counter = 111; counter <= 125; counter++) {
-  if(counter % 2 !== 0) {
-    console.log(counter);
-  }
-}
+console.log('Números ímpares entre 111 e 125:');
+// for (var counter = 111; counter <= 125; counter++) {
+//     if (counter % 2 !== 0) {
+//         console.log(counter);
+//     }
+// }
 
+// Outra resolução:
+for ( var counter = 111; counter <= 125; counter++ ) {
+    counter % 2 !== 0 ? console.log( counter ) : '';
+}
